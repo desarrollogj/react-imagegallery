f52023d528e4381a1d66d08b7d14f84e5ffd35d1
desarrolloglj <desarrolloglj@gmail.com>
2023-07-13 21:52:53 -0300

    Added page names

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from 'react'
 import { Gallery2 } from '../components/Gallery2'
 import { Menu } from '../components/Menu'
 
 export const GalleryPage = () => {
+    useEffect(() => {
+        document.title = "Proyecto Galería de imágenes"
+    }, [])
+
     return (
         <>
             <Menu></Menu>
--- src/pages/ImagePage.jsx
+++ src/pages/ImagePage.jsx
@@ -1,9 +1,13 @@
+import { useEffect } from 'react'
 import { Menu } from "../components/Menu"
 
 export const ImagePage = () => {
     /*useEffect(() => {
         console.log(user);
     }, [])*/
+    useEffect(() => {
+        document.title = "Imagen"
+      }, [])
 
     return (
         <>
